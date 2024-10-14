53fd03308c27d97325af991b55a23c6d4982da00
Alfonso Serrano ED <alfonso.serrano@iestorrevigia.es>
2024-10-14 12:25:49 +0200

    Primer commit de Operaciones Matemáticas

---
+++ src/operacionesmatematicas/OperacionesMatematicas.java
@@ -0,0 +1,42 @@
+
+package operacionesmatematicas;
+
+/**
+ * 
+ * @author Alfonso
+ * @version 1.0 04/10/2024
+ * 
+/**
+ * Clase que contiene métodos para operaciones matemáticas básicas.
+ */
+public class OperacionesMatematicas {
+
+    /**
+     * Suma dos números enteros y devuelve el resultado.
+     *
+     * @param numero1 El primer número a sumar.
+     * @param numero2 El segundo número a sumar.
+     * @return La suma de numero1 y numero2.
+     */
+    
+    public static int sumar(int numero1, int numero2) {
+        return numero1 + numero2;
+    }
+
+    /**
+     * Método principal que ejecuta el programa para sumar dos números.
+     *
+     * @param args Los argumentos de la línea de comandos (no se utilizan aquí).
+     */
+    public static void main(String[] args) {
+        
+        int num1 = 5;
+        int num2 = 3;
+
+        // Llamar al método sumar
+        int resultado = sumar(num1, num2);
+
+        // Imprimir el resultado
+        System.out.println("La suma de " + num1 + " y " + num2 + " es: " + resultado);
+    }
+}
