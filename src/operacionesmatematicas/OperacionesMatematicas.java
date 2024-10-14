0e8bb2895d64c1763c320632221df4fedb620bc6
Alfonso Serrano ED <alfonso.serrano@iestorrevigia.es>
2024-10-14 12:35:32 +0200

    Función restar en Operaciones matemáticas

--- src/operacionesmatematicas/OperacionesMatematicas.java
+++ src/operacionesmatematicas/OperacionesMatematicas.java
@@ -39,8 +39,16 @@ public class OperacionesMatematicas {
 
         // Llamar al método sumar
         int resultado = sumar(num1, num2);
-
+       
+   // Llamar al método restar
+        int resta = restar(num1, num2);
+        
         // Imprimir el resultado
         System.out.println("La suma de " + num1 + " y " + num2 + " es: " + resultado);
+        
+         // Imprimir el resultado
+        System.out.println("La suma de " + num1 + " y " + num2 + " es: " + resta);
+        
+        
     }
 }
